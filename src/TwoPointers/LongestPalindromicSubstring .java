4ac97afdc8b82dec0e0a2004e7ecb5d0acaadc57
Aswin_B.E_CSE <141166978+aswin24012004@users.noreply.github.com>
2026-08-30 23:49:46 +0530

    Add LongestPalindromicSubstring class implementation
    Implemented a method to find the longest palindromic substring using the expand-around-center technique.

--- src/TwoPointers/LongestPalindromicSubstring .java
+++ src/TwoPointers/LongestPalindromicSubstring .java
@@ -1,1 +1,39 @@
-.
+package TwoPointers;
+
+public class LongestPalindromicSubstring {
+    private static int expand(String str, int left, int right) {
+
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+
+    public static String solution(String str) {
+        int start = 0;
+        int end = str.length()-1;
+        int maxLength = 0;
+
+        for (int i = 0; i < end; i++) {
+            int length1 = expand(str, i, i);
+            int length2 = expand(str, i, i+1);
+            int length  = Math.max(length1, length2);
+
+            if (length > maxLength) {
+                start = i - (length-1) / 2;
+                maxLength = length;
+            }
+        }
+
+        return  str.substring(start, start + maxLength);
+    }
+
+    public static void main(String[] args) {
+
+//        String s = "babad";
+        String s = "bsscmadamd";
+        System.out.println(solution(s));
+    }
+}
